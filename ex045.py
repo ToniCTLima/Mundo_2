62b1e1148b70f7ae56bbd6ad6908ae55e288f20f
Toni <tonictlima@gmail.com>
2024-12-04 01:55:30 -0300

    ex045.py
    # Crie um programa que faça o computador jogar jokenpô com você.

--- ex045.py
+++ ex045.py
@@ -1,6 +1,7 @@
 # Crie um programa que faça o computador jogar jokenpô com você.
 
 from random import randint
+from time import sleep
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0,2)
 print('''Suas opções:
@@ -8,41 +9,42 @@ print('''Suas opções:
 [1] PAPEL
 [2] TESOURA ''')
 jogador = int(input('Qual é a sua jogada? '))
-print('-=' * 11)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO')
+print('-=' * 12)
 print(f'O COMPUTADOR jogou {itens[computador]}')
 print(f'O JOGADOR jogou {itens[jogador]}')
-print('-=' * 11)
+print('-=' * 12)
 
 if computador == 0: # computador jogou PEDRA
     if jogador == 0:
-
+        print('EMPATE')
     elif jogador == 1:
-
+        print('JOGADOR VENCE')
     elif jogador == 2:
-
+        print('COMPUTADOR VENCE')
     else:
         print('Jogada inválida.')
 
 elif computador == 1: # computador jogou PAPEL
     if jogador == 0:
-
+        print('COMPUTADOR VENCE')
     elif jogador == 1:
-
+        print('EMPATE')
     elif jogador == 2:
-
+        print('JOGADOR VENCE')
     else:
         print('Jogada inválida.')
 
 elif computador == 2: # computador jogou TESOURA
     if jogador == 0:
-
+        print('JOGADOR VENCE')
     elif jogador == 1:
-
+        print('COMPUTADOR VENCE')
     elif jogador == 2:
-
+        print('EMPATE')
     else:
         print('Jogada inválida.')
-
-
-
-
