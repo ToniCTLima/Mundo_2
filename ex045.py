75ecf98b758a53ab5d438507601c9c234fb484b2
Toni <tonictlima@gmail.com>
2024-12-09 20:14:33 -0300

    Update ex045.py

--- ex045.py
+++ ex045.py
@@ -9,15 +9,21 @@ print('''Suas opções:
 [1] PAPEL
 [2] TESOURA ''')
 jogador = int(input('Qual é a sua jogada? '))
-print('JO')
-sleep(1)
-print('KEN')
-sleep(1)
-print('PO')
-print('-=' * 12)
-print(f'O COMPUTADOR jogou {itens[computador]}')
-print(f'O JOGADOR jogou {itens[jogador]}')
-print('-=' * 12)
+
+if jogador not in [0, 1, 2]:
+    print('Jogada inválida.')
+    
+else:
+
+    print('JO')
+    sleep(1)
+    print('KEN')
+    sleep(1)
+    print('PO')
+    print('-=' * 12)
+    print(f'O COMPUTADOR jogou {itens[computador]}')
+    print(f'O JOGADOR jogou {itens[jogador]}')
+    print('-=' * 12)
 
 if computador == 0: # computador jogou PEDRA
     if jogador == 0:
